b187a199cc1e1c134adc1a4b8c5e928b79e1ab69
projectid1625 <projectid1625@gmail.com>
2025-06-01 09:25:09 +0000

    Connectivity Setup

--- Main.js
+++ Main.js
@@ -1,1 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { spawn } = require('child_process');
 
+function createWindow () {
+  
+    const win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
+                    preload: path.join(__dirname, 'preload.js'), } });
+
+    win.loadFile('index.html');
+
+}
+
+app.whenReady().then(() => {
+                              
+  // Start Python backend
+                                
+  const py = spawn('python', ['python/First_Page.py']);
+
+  py.stdout.on('data', (data) => {
+                                      
+    console.log(`Python says: ${data}`);
+                                      
+  });
+
+  createWindow();
+                                          
+});
